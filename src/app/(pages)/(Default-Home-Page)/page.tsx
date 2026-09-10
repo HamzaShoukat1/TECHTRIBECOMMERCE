75534967faf118c3a0513159fd75953d60ca61d6
hamza <=>
2026-09-10 14:40:06 +0500

    improving in fronted

--- src/app/(pages)/(Default-Home-Page)/page.tsx
+++ src/app/(pages)/(Default-Home-Page)/page.tsx
@@ -118,11 +118,11 @@ export default async function Page() {
 
 
         </div>
-      <Link href={"/shop"}>
-        <button className="text-[#B88E2F] w-full sm:w-fit cursor-pointer    border-[#B88E2F] border-[1px] flex justify-center mx-auto items-center transition-all px-12 py-4  font-poppins font-bold text-sm uppercase tracking-wider rounded-sm">
-          Show More
-        </button>
-      </Link>
+        <Link href={"/shop"}>
+          <button className="text-[#B88E2F] w-full sm:w-fit cursor-pointer    border-[#B88E2F] border-[1px] flex justify-center mx-auto items-center transition-all px-12 py-4  font-poppins font-bold text-sm uppercase tracking-wider rounded-sm">
+            Show More
+          </button>
+        </Link>
 
       </section>
       {/* // furniture picture  */}
