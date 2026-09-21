b2f7a1bbd69ed843a62212335464670ca1beb956
hamza <=>
2026-09-21 20:46:35 +0500

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ export function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
 
     // 1. Check for the authentication token in cookies
-    const hasToken = request.cookies.has("accessToken")
+    const hasToken = request.cookies.has("accessToken")?.valueOf()
 
     const isAuthPage =
         pathname === "/login" ||
