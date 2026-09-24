fb7e4e8ed6b687f6a8d38ba75882ccd7a8cb0608
hamza <=>
2026-09-24 16:42:52 +0500

    changes

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,5 +1,4 @@
 
-// proxy.ts or middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { jwtVerify } from 'jose';
@@ -8,12 +7,17 @@ const protectedRoutes = ['/admin', '/cart', '/shop', '/orders', '/checkout', '/p
 
 export async function proxy(req: NextRequest) {
     const path = req.nextUrl.pathname;
+    console.log(path);
+
+        console.log('🔥 PROXY RUNNING');
+    console.log('PATH:', req.nextUrl.pathname);
     
     const isProtectedRoute = protectedRoutes.some((route) => 
         path === route || path.startsWith(`${route}/`)
     );
 
     if (isProtectedRoute) {
+        console.log(req.cookies);
         const token = req.cookies.get('accessToken')?.value;
 
         if (!token) {
@@ -22,6 +26,7 @@ export async function proxy(req: NextRequest) {
 
         try {
             const secret = new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET!);
+            console.log("zz",secret)
             await jwtVerify(token, secret);
             return NextResponse.next();
         } catch {
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+};
